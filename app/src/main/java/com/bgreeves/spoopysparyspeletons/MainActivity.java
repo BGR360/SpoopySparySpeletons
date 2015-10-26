b7b48fa2c639705276c601c6809b063ee788f61b
Ben Reeves <benwolverine2019@gmail.com>
2015-10-26 16:26:58 -0400

    Added spoopy spary sounds

--- app/src/main/java/com/bgreeves/spoopysparyspeletons/MainActivity.java
+++ app/src/main/java/com/bgreeves/spoopysparyspeletons/MainActivity.java
@@ -1,35 +1,23 @@
 package com.bgreeves.spoopysparyspeletons;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener
 {
+    SoundPlayer spoopySparyPlayer;
+    boolean canSpoopYet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        spoopySparyPlayer = new SoundPlayer(this, R.raw.spoopy_spary_speletons, this);
     }
 
     @Override
@@ -38,6 +26,16 @@ public class MainActivity extends AppCompatActivity
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if (spoopySparyPlayer.isPlaying())
+        {
+            spoopySparyPlayer.stop();
+        }
     }
 
     @Override
@@ -55,5 +53,19 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp)
+    {
+        canSpoopYet = true;
+    }
+
+    public void spoopTheShitOutOfMePls()
+    {
+        if (canSpoopYet)
+        {
+            spoopySparyPlayer.start();
+        }
     }
 }
